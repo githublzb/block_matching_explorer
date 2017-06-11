dc9664a0221836df14801ab3b68b05a2086369b3
Cristóvão Cruz <cristovao@noiselessimaging.com>
2017-06-11 22:58:58 +0300

    Add y to generated mat file. Use smaller ns and n2 for testing.

--- generate_block_match_mat.py
+++ generate_block_match_mat.py
@@ -3,8 +3,8 @@ import block_match as bm
 import scipy.io
 
 n1 = 8    # Block edge
-n2 = 32   # Number of similar blocks to look for
-ns = 32   # Search neighbourhood edge
+n2 = 8   # Number of similar blocks to look for
+ns = 7   # Search neighbourhood edge
 nstep = 7 # Reference block grid step
 
 y = skimage.io.imread('cameraman.tif')
@@ -15,4 +15,6 @@ ref = bm.select_grid_reference(y, n1, ns, nstep)
 # Do the actual block matching.
 mt, d, df = bm.block_match_self_local(y, ref, n1, n2, ns)
 
-scipy.io.savemat('block_match_results', {'mt':mt, 'ref':ref, 'ns':ns, 'nstep':nstep, 'n1':n1, 'n2':n2})
+scipy.io.savemat('block_match_results', {'mt':mt, 'ref':ref, 'ns':ns,
+                                         'nstep':nstep, 'n1':n1, 'n2':n2,
+                                         'y': y})
