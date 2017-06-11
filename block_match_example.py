cc03e06854fff71f6f91c18228463655b18f79e0
Cristóvão Cruz <cristovao@noiselessimaging.com>
2017-06-11 22:04:32 +0300

    Use smaller image for testing, minor adjustments to example.

--- block_match_example.py
+++ block_match_example.py
@@ -6,18 +6,22 @@ n1 = 8   # Block edge
 n2 = 32  # Number of similar blocks to look for
 ns = 32  # Search neighbourhood edge
 
-y = skimage.io.imread('0716.png', as_grey=True)
+y = skimage.io.imread('cameraman.tif')
 
 # Pick a random block as reference.
-rc = bm.select_random_reference(y, n1, ns, 5)
+# ref = bm.select_random_reference(y, n1, ns, 5)
+
+# Pick all blocks on a grid.
+nstep = 7
+ref = bm.select_grid_reference(y, n1, ns, nstep)
 
 # Do the actual block matching and build groups.
 # mt, d, df = block_match.block_match_self_global(y, rc, n1, n2)
-mt, d, df = bm.block_match_self_local(y, rc, n1, n2, ns)
+mt, d, df = bm.block_match_self_local(y, ref, n1, n2, ns)
 
-bm.visualise_match_table(mt, y, n1, rc, d, df)
+bm.visualise_match_table(mt, y, n1, ref, d, df)
 
-ref, cdt, lab = bm.read_labelled_group(y, mt, rc, n1, ns)
+ref, cdt, lab = bm.read_labelled_group(y, mt, ref, n1, ns)
 
 plt.imshow(lab[0].reshape(ns, ns))
 plt.show()
